b6c7c74cd0e7bcc3375701edc84332c8cf3554db
Isaac <caldas.isaac@gmail.com>
2020-11-08 19:10:30 -0300

    Corrected WhiteList to the lastest version

--- src/main/java/br/edu/unifei/gpesc/core/antispam/WhiteList.java
+++ src/main/java/br/edu/unifei/gpesc/core/antispam/WhiteList.java
@@ -21,6 +21,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -29,20 +30,31 @@ import java.util.HashSet;
  */
 public class WhiteList {
 	
-    private final HashSet<string> mAllowed;
+    private final String mFolder;
+    private final HashMap<String, HashSet<String>> mWhiteLists = new HashMap<String, HashSet<String>>();
 
-    public WhiteList(String file) {
+    public WhiteList(String folder) {
+        mFolder = folder;
         mAllowed = loadData(file);
     }
 
-    private static HashSet<string> loadData(String file) {
-        HashSet<String> allowed = new HashSet<String>();
+    private HashSet<String> getUserWhiteList(String userMail) {
+        if (mWhiteLists.containsKey(userMail)) {
+            return mWhiteLists.get(userMail);
+        }
         
-        if (new File(file).exists()) {
+        // Open
+        File file = new File(mFolder, userMail);
+        HashSet<String> whiteList = null;
+        
+        if (file.exists()) 
+        {
+            whiteList = new HashSet<String>();
+            
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while ((line = br.readLine()) != null) {
-                   allowed.add(line);
+                   whiteList.add(line);
                 }
             }
             catch (IOException e) {
@@ -50,10 +62,12 @@ public class WhiteList {
             }
         }
 
-        return allowed;
+        mWhiteLists.put(userMail, whiteList);
+        return whiteList;
     }
 
-    public bool isWhiteListed(string from) {
-            return mAllowed.contains(from);
+    public bool isWhiteListed(String from, String to) {
+        HashSet<String> whiteList = getUserWhiteList(to);
+        return whiteList != null && whiteList.contains(from);
     }
 }
